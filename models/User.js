7e1de852ce8bf1118e08b418661ae1d455506494
piyush-c38 <chandrakarpiyush36@gmail.com>
2025-05-04 17:25:33 +0530

    FIX: fixed the bcrypt bug

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -12,13 +12,13 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcryptjs.hash(this.password, 10);
     next();
 });
 
 // Method to compare passwords
 userSchema.methods.comparePassword = function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+    return bcryptjs.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
